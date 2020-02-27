d8208b78f5931a757ec358aa0747d1b59acea13e
liamconns <43392077+liamconns@users.noreply.github.com>
2020-02-27 14:50:16 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+import random
+from random import randint
+
+
+print("Hello! Welcome to the Number guessing game")
+
+playAgain = True
+
+while playAgain == True:
+
+    print("Guess a number between 1 and 100!")
+
+    userAnswer = False
+    randomNumber = randint(1, 100)
+
+
+    while userAnswer == False:
+
+        userInput = int(input())
+
+        if userInput == randomNumber:
+
+            userAnswer = True
+
+        elif userInput < randomNumber:
+
+            print("Too low!!")
+
+        else:
+
+            print("Too high!!")
+
+
+            
+
+    print("Congrats!! You Win")
+    print("Do you want to play again? Select y or n")
+
+    again = input()
+
+    if again == 'n':
+
+        print("Thanks for playinG!")
+
+        break
+
+    
+
+
